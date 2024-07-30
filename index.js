51d38b4ef987f7419f6218a7147cd0a73f21b431
Yashwanth Chowdary <yashwanthchowdary@Yashwanths-MBP.lan>
2024-07-30 12:27:05 -0500

    hi

---
+++ index.js
@@ -0,0 +1,29 @@
+/*class MyArray {
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        console.log("-------", this.data);
+    }
+}
+
+const myNewArray = new MyArray();
+console.log(myNewArray.push("apple"));
+*/
+
+
+/*const groceries = ["milk","bread","eggs","flour","choose","sugar"];
+
+const searchForItem = (item) => {
+    for(let i = 0; i < groceries.length;i++){
+        if(groceries[i]=== item){
+            console.log(`Found: ${item}`);
+        }
+    }
+};
+
+searchForItem("eggs");*/
+
